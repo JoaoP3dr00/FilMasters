e0240afe258fc1e74a76b19d9e33fc5a737c3d64
JoaoP3dr00 <rjoao.pedro05@gmail.com>
2025-06-01 11:06:30 -0400

    ci: adicionando configuração inicial da pipeline do ci/cd para a main

--- src/main/java/com/gcs/FilMasters/FilMastersApplication.java
+++ src/main/java/com/gcs/FilMasters/FilMastersApplication.java
@@ -2,11 +2,13 @@ package com.gcs.FilMasters;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class FilMastersApplication {
 
 	public static void main(String[] args) {
+		System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "");
 		SpringApplication.run(FilMastersApplication.class, args);
 	}
 
--- src/test/java/com/gcs/FilMasters/FilMastersApplicationTests.java
+++ src/test/java/com/gcs/FilMasters/FilMastersApplicationTests.java
@@ -2,12 +2,13 @@ package com.gcs.FilMasters;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootTest
 class FilMastersApplicationTests {
-
 	@Test
-	void contextLoads() {
+	void mainTest() {
+		//System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "");
+		FilMastersApplication.main(new String[]{});
 	}
-
 }
