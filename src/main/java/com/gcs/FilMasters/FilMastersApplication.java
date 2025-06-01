b97536e144b656307766acfe6edcd81c64237890
JoaoP3dr00 <rjoao.pedro05@gmail.com>
2025-05-31 21:34:24 -0400

    env: adicionando estrutura inicial do spring boot

---
+++ src/main/java/com/gcs/FilMasters/FilMastersApplication.java
@@ -0,0 +1,13 @@
+package com.gcs.FilMasters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilMastersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilMastersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gcs/FilMasters/FilMastersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcs.FilMasters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilMastersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
