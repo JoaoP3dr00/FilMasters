66bafd3ac8a0f5011377a3a11e7c0b87603e3ba0
JoaoP3dr00 <rjoao.pedro05@gmail.com>
2025-06-01 19:07:55 -0400

    test: adicionando teste de unidade para a entidade movie

---
+++ src/test/java/com/gcs/FilMasters/domain/MovieTest.java
@@ -0,0 +1,26 @@
+package com.gcs.FilMasters.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MovieTest {
+    @Test
+    public void instantiateNewMovieTest(){
+        // Arrange
+        final String id = "1";
+        final String nome = "Godzilla";
+        final String descricao = "Monstro japones.";
+
+        // Act
+        Movie movie = new Movie(id, nome, descricao);
+
+        // Assert
+        Assertions.assertNotNull(movie);
+        Assertions.assertNotNull(movie.getId());
+        Assertions.assertNotNull(movie.getNome());
+        Assertions.assertNotNull(movie.getDescricao());
+        Assertions.assertEquals(id, movie.getId());
+        Assertions.assertEquals(nome, movie.getNome());
+        Assertions.assertEquals(descricao, movie.getDescricao());
+    }
+}
