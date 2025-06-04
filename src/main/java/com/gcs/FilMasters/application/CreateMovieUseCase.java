df540fc30544676dd4331fef866fa1d2a5b52572
CaioTome <felipe3005cf@gmail.com>
2025-06-04 16:40:35 -0400

    Adição do Create Movie UC. 'Ainda sem o hotfix'

--- src/main/java/com/gcs/FilMasters/application/CreateMovieUseCase.java
+++ src/main/java/com/gcs/FilMasters/application/CreateMovieUseCase.java
@@ -1,4 +1,21 @@
 package com.gcs.FilMasters.application;
 
+import org.springframework.stereotype.Service;
+
+import com.gcs.FilMasters.application.persistence.MovieRepository;
+import com.gcs.FilMasters.domain.Movie;
+
+@Service
 public class CreateMovieUseCase {
+
+    private final MovieRepository movieRepository;
+
+    public CreateMovieUseCase(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    public Movie createMovie(String id, String nome, String descricao, int duracaoMinutos) {
+        Movie movie = new Movie(id, nome, descricao, duracaoMinutos);
+        return movieRepository.save(movie);
+    }
 }
